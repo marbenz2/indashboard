cfcd3aa3bb360efa1e28278fc76637d92caa83fc
marbenz2 <benzinger.marcel@gmail.com>
2024-06-27 17:16:24 +0200

    add card ui item

---
+++ src/components/ui/CardDashboard.tsx
@@ -0,0 +1,7 @@
+import { Card } from "@tremor/react";
+
+const CardDashboard = ({ children }: { children: React.ReactNode }) => {
+  return <Card>{children}</Card>;
+};
+
+export default CardDashboard;
