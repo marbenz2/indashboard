b56466853a9a556d444d626f5bb5d1430b59585e
marbenz2 <benzinger.marcel@gmail.com>
2024-06-27 17:16:00 +0200

    add area chart and dummy data

--- src/data/data.ts
+++ src/data/data.ts
@@ -16,3 +16,67 @@ export const data = [
     type: "CardWelcome",
   },
 ];
+
+export const cashdata = [
+  {
+    date: "Jan",
+    "user 1": 2890,
+    "user 2": 2338,
+  },
+  {
+    date: "Feb",
+    "user 1": 2756,
+    "user 2": 2103,
+  },
+  {
+    date: "Mar",
+    "user 1": 3322,
+    "user 2": 2194,
+  },
+  {
+    date: "Apr",
+    "user 1": 3470,
+    "user 2": 2108,
+  },
+  {
+    date: "May",
+    "user 1": 3475,
+    "user 2": 1812,
+  },
+
+  {
+    date: "Jun",
+    "user 1": 3129,
+    "user 2": 1726,
+  },
+  {
+    date: "Jul",
+    "user 1": 3490,
+    "user 2": 1982,
+  },
+  {
+    date: "Aug",
+    "user 1": 2903,
+    "user 2": 2012,
+  },
+  {
+    date: "Sep",
+    "user 1": 2643,
+    "user 2": 2342,
+  },
+  {
+    date: "Oct",
+    "user 1": 2837,
+    "user 2": 2473,
+  },
+  {
+    date: "Nov",
+    "user 1": 2954,
+    "user 2": 3848,
+  },
+  {
+    date: "Dec",
+    "user 1": 3239,
+    "user 2": 3736,
+  },
+];
