47684cfcb8a0f6bfde89c3cbb98281533192eb87
marbenz2 <benzinger.marcel@gmail.com>
2024-07-01 23:08:13 +0200

    removed old components

--- src/components/ui/CardDashboard.tsx
+++
@@ -1,7 +0,0 @@
-import { Card } from "@tremor/react";
-
-const CardDashboard = ({ children }: { children: React.ReactNode }) => {
-  return <Card>{children}</Card>;
-};
-
-export default CardDashboard;
