42fa2ae0a6da0f5bcf7deff1e74bbd6cf1bb9424
marbenz2 <benzinger.marcel@gmail.com>
2024-07-01 23:11:24 +0200

    added utility functions

--- src/lib/utils.ts
+++ src/lib/utils.ts
@@ -4,3 +4,11 @@ import { twMerge } from "tailwind-merge";
 export function cn(...inputs: ClassValue[]) {
   return twMerge(clsx(inputs));
 }
+
+export function valueFormatter(number: number) {
+  return new Intl.NumberFormat("de").format(number).toString();
+}
+
+export function capitalizeFirstLetter(string: string) {
+  return string.charAt(0).toUpperCase() + string.slice(1);
+}
